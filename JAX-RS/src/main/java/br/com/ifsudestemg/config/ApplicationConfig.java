ddb543c0a866fb1a1efc7e944d817a7c2cb15b42
Carlos Rodrigues <77905915+carlosrdrgs@users.noreply.github.com>
2026-02-21 16:02:36 -0300

    Delete JAX-RS directory

--- JAX-RS/src/main/java/br/com/ifsudestemg/config/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package br.com.ifsudestemg.config;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class ApplicationConfig extends Application {
-}
